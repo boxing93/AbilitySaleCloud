a2ebf60d110c6d731ce102c041e663974d06d04f
i.valchev <i.valchev@eltrade.com>
2019-11-29 17:54:00 +0200

    Refactored IdentityService

---
+++ src/AbilitySaleCloud.IdentityService/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AbilitySaleCloud.Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetLoggedUsers()
+        {
+            return Ok("10 users are logged in");
+        }
+            
+    }
+}

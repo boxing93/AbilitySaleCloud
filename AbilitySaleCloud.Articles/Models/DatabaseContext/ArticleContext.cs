029d1d11a636b033427961602fcc6f6459aacf7c
i.valchev <i.valchev@eltrade.com>
2019-11-30 15:06:21 +0200

    Added Articles Project and EF Core for SQL server

---
+++ AbilitySaleCloud.Articles/Models/DatabaseContext/ArticleContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbilitySaleCloud.Articles.Models.DatabaseContext
+{
+    public class ArticleContext : DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+        {
+        }
+    }
+}
